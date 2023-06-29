ac0a244d8ec1f2de6d393091dd7af84499ade484
miroswd <altamirsantos93@hotmail.com>
2023-06-29 20:01:36 -0300

    fix: invert methods delete and update

--- models/delete.go
+++ models/delete.go
@@ -2,7 +2,7 @@ package models
 
 import "github.com/miroswd/go-api/db"
 
-func Delete(id int64, todo Todo) (int64, error) {
+func Delete(id int64) (int64, error) {
 	connection, err := db.OpenConnection()
 
 	if err != nil {
@@ -11,7 +11,7 @@ func Delete(id int64, todo Todo) (int64, error) {
 
 	defer connection.Close()
 
-	res, err := connection.Exec(`UPDATE todos SET title=$1, description=$2, done=$3 WHERE id=$4`, todo.Title, todo.Description, todo.Done, id)
+	res, err := connection.Exec(`DELETE FROM todos WHERE id=$1`, id)
 
 	if err != nil {
 		return 0, err
